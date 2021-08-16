08236f902b3f316fcebeb3feddf760af7666a091
Tommy Veen <75741894+tommyveen@users.noreply.github.com>
2021-08-16 10:53:26 +0200

    Add files via upload

---
+++ JavaScript_019/main.js
@@ -0,0 +1,48 @@
+// const superheroes = [
+//     {name: "Batman", alter_ego: "Bruce Wayne"}, 
+//     {name: "Superman", alter_ego: "Clark Kent"}, 
+//     {name: "Spiderman", alter_ego: "Peter Parker"}]
+    
+// let searchedItem = superheroes.find(findSpiderMan);
+
+// function findSpiderMan(element){
+//         return element.name == "Spiderman"
+// }
+    
+// console.log(searchedItem);
+
+// **
+
+// const doubleArrayValues = ([1, 2, 3]) 
+
+
+// doubleArrayValues.forEach(consoleItem);
+
+// function consoleItem(item) {
+//     console.log(item * 2)
+// }
+
+// **
+
+// const containsNumberBiggerThan10 = ([1, 4, 3, 6, 9, 7, 11]) 
+
+// containsNumberBiggerThan10.find(consoleItem)
+
+// function consoleItem(item) {
+//     console.log(item > 10)
+// };
+
+// console.log(found(containsNumberBiggerThan10));
+
+const containsNumberBiggerThan10 = ([1,2,1,2,1,2])
+
+containsNumberBiggerThan10.find(consoleItem)
+
+function consoleItem(item) {
+    console.log(item > 10)
+};
+
+// result should be true
+// containsNumberBiggerThan10([1,2,1,2,1,2]})
+// result should be false
+
